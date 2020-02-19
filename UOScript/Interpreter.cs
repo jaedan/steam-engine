986df26346f4af7cf94795adcc026c86ab547a5b
roxya <st@roxya.com>
2020-02-19 00:12:08 +0000

    return timespan and allow api user to decide what to do with it

--- UOScript/Interpreter.cs
+++ UOScript/Interpreter.cs
@@ -1202,14 +1202,14 @@ namespace UOScript
             _timers[name] = DateTime.UtcNow;
         }
 
-        public static int GetTimer(string name)
+        public static TimeSpan GetTimer(string name)
         {
             if (!_timers.TryGetValue(name, out DateTime timestamp))
                 throw new RunTimeError(null, "Timer does not exist");
 
             TimeSpan elapsed = DateTime.UtcNow - timestamp;
 
-            return (int)elapsed.TotalMilliseconds;
+            return elapsed;
         }
 
         public static void SetTimer(string name, int elapsed)
